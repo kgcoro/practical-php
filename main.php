00a33437aeb35ce4c0d63b7812a0ac60ad930e17
Kleber Coro <kgcoro@gmail.com>
2022-09-14 21:54:29 -0500

    Solucion nivel 2

--- main.php
+++ main.php
@@ -1,21 +1,18 @@
 <?php
 
-$palabras = array("sol", "luna", "cielo");
+$palabras = array("sol", "luna", "cielo", "luz", "estrellas", "lluvia");
 $palabrasDesordenadas = array();
 
-for ($i=0; $i < 3; $i++) { 
-    $palabrasDesordenadas[$i] = str_shuffle($palabras[$i]);
+$form = "<form action='analisis.php'>";
+for ($i=0; $i < count($palabras); $i++) { 
+    $form .= "La palabra: " . str_shuffle($palabras[$i]) . " " .
+    "<input type='text' name='palabra".[$i]."'>" . 
+    "<br>";
 }
 
-print_r($palabrasDesordenadas);
+$button = "<button type='submit'>Enviar</button>";
+$formCierre = "</form>";
 
-echo "
-<form action='analisis.php'>
-    <input type='text' name='palabra0'>
-    <input type='text' name='palabra1'>
-    <input type='text' name='palabra2'>
-    <button type='submit'>Enviar</button>
-</form>
-";
-
+echo $form . $button . $formCierre;
+ 
 ?>
